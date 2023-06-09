f681109a27ae7009c2f068daf1ff14ecb175105d
Ty Andrews <ty.elgin.andrews@gmail.com>
2023-06-08 22:28:37 -0700

    feat: added better logging to deploy script

--- src/deployment/price_prediction_api/deploy.py
+++ src/deployment/price_prediction_api/deploy.py
@@ -8,15 +8,28 @@ from azureml.core import Workspace
 from azureml.core.model import InferenceConfig
 from azureml.core.webservice import AciWebservice
 from azureml.core.model import Model
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+PRICE_PREDICTION_MODEL_VER = 3
+PRICE_PREDICTION_MODEL_QUANT5_VER = 1
+PRICE_PREDICTION_MODEL_QUANT95_VER = 1
+
+CPU_CORES = 0.5
+MEMORY_GB = 2
 
 
 def main():
+    logger.info("Loading workspace from config.json")
     ws = Workspace.from_config(
         # assumed running from root of repo
         path=os.path.join("src", "deployment", "price_prediction_api", "config.json")
     )
     print(ws.name, ws.resource_group, ws.location, ws.subscription_id, sep="\n")
 
+    logger.info("Creating Azure environment")
     env = Environment("price-prediction-env")
     env.python.conda_dependencies.add_pip_package("joblib")
     env.python.conda_dependencies.add_pip_package("pandas==1.5")
@@ -32,14 +45,32 @@ def main():
         environment=env,
     )
 
-    price_model = Model(ws, "all-vehicles-price-prediction", version=3)
-    price_model_quant5 = Model(ws, "all-vehicles-price-prediction-quant5", version=1)
-    price_model_quant95 = Model(ws, "all-vehicles-price-prediction-quant95", version=1)
-
-    deployment_config = AciWebservice.deploy_configuration(
-        cpu_cores=0.5, memory_gb=2, enable_app_insights=True
+    logger.info(f"Loading models from workspace.")
+    logger.info(
+        f"Model Versions: price-prediction model ver: {PRICE_PREDICTION_MODEL_VER}, price-prediction-quant5 model ver: {PRICE_PREDICTION_MODEL_QUANT5_VER}, price-prediction-quant95 model ver: {PRICE_PREDICTION_MODEL_QUANT95_VER}"
+    )
+    price_model = Model(
+        ws, "all-vehicles-price-prediction", version=PRICE_PREDICTION_MODEL_VER
+    )
+    price_model_quant5 = Model(
+        ws,
+        "all-vehicles-price-prediction-quant5",
+        version=PRICE_PREDICTION_MODEL_QUANT5_VER,
+    )
+    price_model_quant95 = Model(
+        ws,
+        "all-vehicles-price-prediction-quant95",
+        version=PRICE_PREDICTION_MODEL_QUANT95_VER,
     )
 
+    logger.info(
+        f"Creating deployment configuration with {CPU_CORES} CPU cores and {MEMORY_GB} GB memory"
+    )
+    deployment_config = AciWebservice.deploy_configuration(
+        cpu_cores=CPU_CORES, memory_gb=MEMORY_GB, enable_app_insights=True
+    )
+
+    logger.info("Deploying model to Azure Container Instance (ACI)")
     service = Model.deploy(
         ws,
         "price-prediction-service",
@@ -48,10 +79,9 @@ def main():
         deployment_config,
         overwrite=True,
     )
-
     service.wait_for_deployment(show_output=True)
 
-    print("Deployment done, scoring URI: ", service.scoring_uri)
+    logger.info("Deployment done, scoring URI: ", service.scoring_uri)
 
 
 if __name__ == "__main__":
