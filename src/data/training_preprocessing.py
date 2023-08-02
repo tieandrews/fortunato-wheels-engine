b85db83ab7b4a949ce343e8909248ba3abad7c08
Ty Andrews <ty.elgin.andrews@gmail.com>
2023-08-02 14:42:55 -0600

    feat: added standard options and filtering options

--- src/data/training_preprocessing.py
+++ src/data/training_preprocessing.py
@@ -28,6 +28,22 @@ def preprocess_ads_for_training(
     max_age_at_posting=20,
     min_price=1000,
     max_price=250000,
+    major_options = [
+        "backup-camera",
+        "bluetooth",
+        "heated-seats",
+        "navigation-system",
+        "sunroof-moonroof",
+        "leather-seats",
+        "remote-start",
+        "carplay-android-auto",
+        "blind-spot-monitoring-assist",
+        "adaptive-cruise-control",
+        "cruise-control",
+        "third-row-seating",
+        "air-conditioning",
+        "none-listed", # for ads with no options entered
+    ]
 ):
     """
     Preprocess a pandas DataFrame containing car ads for training.
@@ -78,10 +94,6 @@ def preprocess_ads_for_training(
         )
 
     if "mileage_per_year" in model_features:
-        # where ads have an age_at_posting of zero set mileage_per_year to mileage
-        preprocessed_df.loc[
-            preprocessed_df["age_at_posting"] == 0, "mileage_per_year"
-        ] = preprocessed_df.loc[preprocessed_df["age_at_posting"] == 0, "mileage"]
         # drop any other mileage per year NaNs
         preprocessed_df = preprocessed_df[~preprocessed_df["mileage_per_year"].isna()]
 
@@ -93,6 +105,35 @@ def preprocess_ads_for_training(
                 & (preprocessed_df["mileage_per_year"] <= 500)
             )
         ]
+
+    if "options_list" in model_features:
+
+        # create uniique id for use when exploding then regrouping options list
+        if "unique_id" not in preprocessed_df.columns:
+            preprocessed_df = preprocessed_df.reset_index().rename(columns={"index": "unique_id"})
+
+        preprocessed_df = preprocessed_df.explode("options_list")
+
+        preprocessed_df = preprocessed_df[
+            (preprocessed_df["options_list"].isin(major_options))
+            | (preprocessed_df["options_list"].isna())
+        ]
+
+        preprocessed_df = (
+            preprocessed_df.groupby(by="unique_id", as_index=False)
+            .agg(
+                {
+                    "options_list": list,
+                    **{
+                        col: "first" for col in preprocessed_df.columns if col != "options_list"
+                    },
+                }
+            )
+            .reset_index(drop=True)
+        )
+
+        # drop the unique_id field because it is no longer needed
+        preprocessed_df = preprocessed_df.drop(columns=["unique_id"])
 
     # ensure only unique make/model combos are kept, some car models share names across makes
     # inspired from: https://stackoverflow.com/questions/35268817/unique-combinations-of-values-in-selected-columns-in-pandas-data-frame-and-count
