4dc7fbff75c17a10ca69d84573ba3820d043b1ed
Ty Andrews <ty.elgin.andrews@gmail.com>
2023-09-17 14:11:14 -0600

    feat: gpu support and formatting

--- src/training/training.py
+++ src/training/training.py
@@ -15,11 +15,17 @@ import hydra
 from hydra.core.hydra_config import HydraConfig
 from omegaconf import DictConfig, OmegaConf
 import hyperopt as hp
+import torch
 import git
 
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import OneHotEncoder, StandardScaler, MultiLabelBinarizer, LabelEncoder
+from sklearn.preprocessing import (
+    OneHotEncoder,
+    StandardScaler,
+    MultiLabelBinarizer,
+    LabelEncoder,
+)
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.compose import ColumnTransformer
 from sklearn.linear_model import Ridge
@@ -46,10 +52,19 @@ from src.training.custom_components import MultiHotEncoder
 
 load_dotenv(find_dotenv())
 
+logger = get_logger(__name__)
+
+# Check if CUDA is available
+if torch.cuda.is_available():
+    logger.info("CUDA is available, running on GPU.")
+    device = "cuda"
+else:
+    logger.info("CUDA is not available, running on CPU.")
+    device = "cpu"
+
 # environment variable to prevent timeout on upload of large files
 os.environ["AZUREML_ARTIFACTS_DEFAULT_TIMEOUT"] = "3600"
 
-logger = get_logger(__name__)
 
 AZURE_MLFLOW_URI = os.environ.get("AZURE_MLFLOW_URI")
 mlflow.set_tracking_uri(AZURE_MLFLOW_URI)
@@ -65,17 +80,17 @@ def training(cfg: DictConfig) -> None:
     # ads.get_car_ads(sources=["cargurus", "kijiji"])
     if cfg.load_data.params.data_dump is None:
         ads.get_car_ads(
-        make=cfg.load_data.params.make,
-        model=cfg.load_data.params.model,
-        year_range=cfg.load_data.params.year_range,
-        limit_ads=cfg.load_data.params.limit_ads,
-        sources=OmegaConf.to_object(cfg.load_data.params.sources),
-                        )
+            make=cfg.load_data.params.make,
+            model=cfg.load_data.params.model,
+            year_range=cfg.load_data.params.year_range,
+            limit_ads=cfg.load_data.params.limit_ads,
+            sources=OmegaConf.to_object(cfg.load_data.params.sources),
+        )
     else:
         ads.get_car_ads(
             data_dump=cfg.load_data.params.data_dump,
             limit_ads=cfg.load_data.params.limit_ads,
-            )
+        )
 
     model_features = OmegaConf.to_object(
         cfg.preprocess.model_feats.target
@@ -89,13 +104,14 @@ def training(cfg: DictConfig) -> None:
 
     # preprocess ads for training
     preprocessed_ads = preprocess_ads_for_training(
-        ads.df, model_features=model_features,
-                exclude_new_vehicle_ads=cfg.preprocess.exclude,
-                min_num_ads=cfg.preprocess.min_num,
-                max_age_at_posting=cfg.preprocess.max_age,
-                min_price=cfg.preprocess.min_price,
-                max_price=cfg.preprocess.max_price,
-                )
+        ads.df,
+        model_features=model_features,
+        exclude_new_vehicle_ads=cfg.preprocess.exclude,
+        min_num_ads=cfg.preprocess.min_num,
+        max_age_at_posting=cfg.preprocess.max_age,
+        min_price=cfg.preprocess.min_price,
+        max_price=cfg.preprocess.max_price,
+    )
 
     train_df, test_df = train_test_split(
         preprocessed_ads,
@@ -128,25 +144,24 @@ def training(cfg: DictConfig) -> None:
 
     # hyperopt setup
     def objective(params, log_to_mlflow=True):
-        classifier_type = params['type']
-        del params['type']
-        if classifier_type == 'gradient_boosting':
+        classifier_type = params["type"]
+        del params["type"]
+        if classifier_type == "gradient_boosting":
             clf = GradientBoostingRegressor(**params)
-        elif classifier_type == 'xgboost':
-            clf = XGBRegressor(**params)
-        elif classifier_type == 'rf':
+        elif classifier_type == "xgboost":
+            clf = XGBRegressor(device=device, **params)
+        elif classifier_type == "rf":
             clf = RandomForestRegressor(**params)
-        elif classifier_type == 'ridge':
+        elif classifier_type == "ridge":
             clf = Ridge(**params)
         else:
             return 0
-        
+
         repo = git.Repo(search_parent_directories=True)
         hash = repo.git.rev_parse(repo.head, short=True)
 
         # log metrics to mlflow
         with mlflow.start_run(nested=True):
-
             mlflow.set_tag("git_commit", hash)
 
             # log params
@@ -161,7 +176,7 @@ def training(cfg: DictConfig) -> None:
                 ]
             )
 
-            metrics =  OmegaConf.to_object(cfg.hyperopt.metrics)
+            metrics = OmegaConf.to_object(cfg.hyperopt.metrics)
             # manually run cross_validate and get train/test rmse, mape, and r2
             model_cv_results = (
                 pd.DataFrame(
@@ -170,17 +185,17 @@ def training(cfg: DictConfig) -> None:
                         X_train,
                         y_train,
                         cv=5,
-                        scoring=metrics, 
+                        scoring=metrics,
                         return_train_score=True,
                         n_jobs=-1,
-                        verbose=1
+                        verbose=1,
                     )
                 )
                 .agg(["mean", "std"])
                 .T
             )
-            
-            logger.info('Cross validation complete')
+
+            logger.info("Cross validation complete")
 
             # log train and test for each metric
             for m in metrics:
@@ -193,36 +208,36 @@ def training(cfg: DictConfig) -> None:
                 mlflow.log_metric(
                     f"{m}_train_std", model_cv_results.loc[f"train_{m}"]["std"]
                 )
-                mlflow.log_metric(f"{m}_test_std", model_cv_results.loc[f"test_{m}"]["std"])
+                mlflow.log_metric(
+                    f"{m}_test_std", model_cv_results.loc[f"test_{m}"]["std"]
+                )
 
             fit_model = pipe.fit(X_train, y_train)
 
             # log model
             if cfg.mlflow.log_fit_model:
                 mlflow.sklearn.log_model(
-                fit_model,
-                "model",
-                signature=infer_signature(X_train, y_train),
+                    fit_model,
+                    "model",
+                    signature=infer_signature(X_train, y_train),
                 )
 
             # predict on test set
             y_pred = fit_model.predict(X_test)
 
             # add predicted price to test_df, round to 1 decimal place
-            full_df = (
-                test_df.copy(deep=True).assign(predicted_price=y_pred.round(1))
-            )
+            full_df = test_df.copy(deep=True).assign(predicted_price=y_pred.round(1))
 
             # calculate evaluation metrics by model
-            metrics_by_model = price_model.calculate_evaluation_metrics_by_model(full_df)
+            metrics_by_model = price_model.calculate_evaluation_metrics_by_model(
+                full_df
+            )
 
             # calculate evaluation metrics by make
             metrics_by_make = price_model.calculate_evaluation_metrics_by_make(full_df)
 
             # calculate training data metrics
-            train_data_metrics = price_model.calculate_train_data_metrics(
-                train_df
-            )
+            train_data_metrics = price_model.calculate_train_data_metrics(train_df)
 
             with tempfile.TemporaryDirectory() as tmpdir:
                 # Save model metrics to CSV file
@@ -243,7 +258,6 @@ def training(cfg: DictConfig) -> None:
             # log hydra outputs to mlflow
             hydra_path = HydraConfig.get().runtime.output_dir
             mlflow.log_artifacts(hydra_path, artifact_path="hydra_logs/")
-
 
         # make negative mape positive so it minimizes it
         result = {
@@ -275,17 +289,18 @@ def training(cfg: DictConfig) -> None:
     rstate = np.random.default_rng(cfg.hyperopt.seed)
 
     try:
-        with mlflow.start_run(): 
-
+        with mlflow.start_run():
             best_hyperparams = fmin(
                 fn=objective,
                 space=search_space,
                 algo=search_algorithm,
                 max_evals=cfg.mlflow.evals,
                 trials=Trials(),
-                rstate=rstate
+                rstate=rstate,
             )
-            logger.info(f"Finished hyperparameter tuning, best hyperparameters are: {best_hyperparams}")
+            logger.info(
+                f"Finished hyperparameter tuning, best hyperparameters are: {best_hyperparams}"
+            )
             log_file_path = find_logger_basefilename(logger)
             mlflow.log_artifact(log_file_path)
 
